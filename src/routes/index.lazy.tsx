37ace658908ab3a4a6d14e26aaf604ce60efa28a
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-20 01:32:08 +0900

    Feat : nowPlaying TanstackQuery 적용, 테스트 패칭

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,10 +1,25 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { apis, queryKeys } from "utils/apis";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { E_LANGUAGE, E_ORDER } from "types/common";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const [lang] = useState<E_LANGUAGE>(E_LANGUAGE.ko);
+  const [order] = useState<E_ORDER>(E_ORDER.desc);
+
+  const { data, isLoading } = useQuery({
+    queryKey: [queryKeys.nowPlaying, { lang, order }],
+    queryFn: apis.nowPlaying,
+  });
+
+  console.log(isLoading);
+  console.log(data);
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
