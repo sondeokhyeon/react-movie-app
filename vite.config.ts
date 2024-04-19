da02061c38ed07522e0a2d03cc7dfd84223c09ad
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-20 00:00:29 +0900

    Add : Tanstack Router 세팅용 파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
   test: {
     globals: true,
     environment: "jsdom",
