444fa0ad50b5c44a60f526a1a13f943dc118581e
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-20 00:01:04 +0900

    Feat : Tanstack Router 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 import "@picocss/pico";
 import "./styles/index.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const router = createRouter({ routeTree });
+const queryclient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryclient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
