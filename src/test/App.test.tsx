79e6c144ef4b088e2ec8d6e6c4fdb84f65f45cdf
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-19 23:58:41 +0900

    Remove : 기본 테스트 삭제

--- src/test/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App.tsx";
-
-it("test App", () => {
-  render(<App />);
-  const message = screen.getByText(/hello world/);
-  expect(message).toBeInTheDocument();
-});
