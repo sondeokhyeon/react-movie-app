c3cdefa6d6006116332442ce30a30631d59210f3
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-20 21:50:46 +0900

    Feat : FullScreen용 Loading개선

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,7 @@
 export const FullLoading = () => (
-  <div className="h-full flex items-center justify-center" aria-busy="true" />
+  <div className="h-[100vh] w-full ">
+    <div className="h-full flex items-center justify-center" aria-busy="true" />
+  </div>
 );
 
 export const InlineLoading = () => {
