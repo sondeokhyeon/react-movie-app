4127a635c83c990f6fc84af862535d7ac9e2cac8
sondeokhyeon <sondeokhyeon@gmail.com>
2024-04-20 21:49:40 +0900

    Feat : 네비게이션 반응형 대응

--- src/components/Common.tsx
+++ src/components/Common.tsx
@@ -15,58 +15,38 @@ export const IndexNavigation = ({
   };
   return (
     <div className="bg-white w-full h-16 sticky top-0 shadow-[0px_0px_5px_0px_rgba(0,0,0,0.55)]">
-      <div className="flex justify-center items-center h-full gap-5">
+      <div className="flex flex-col md:flex-row md:gap-5 justify-center items-center h-full gap-1">
         <div className="flex">
-          <div>
-            <div>{HP_MULTI_LANG_HANDLER(lang, "언어", "Language")} </div>
+          <div className="flex-1">
+            {HP_MULTI_LANG_HANDLER(lang, "언어", "Language")}
           </div>
-          <div>
-            <select
-              name="favorite-cuisine"
-              aria-label="언어 설정"
-              required
-              onChange={(e) => handleChangeSelect("lang", e.target.value)}
-            >
-              <option disabled value="ko">
-                {HP_MULTI_LANG_HANDLER(
-                  lang,
-                  "언어를 선택하세요.",
-                  "Choose a Language",
-                )}
-              </option>
-              <option value="ko" selected={lang === LANGUAGE.ko}>
-                한국어
-              </option>
-              <option value="en" selected={lang === LANGUAGE.en}>
-                ENGLISH
-              </option>
-            </select>
-          </div>
+          <select
+            className="flex-1"
+            aria-label=""
+            defaultValue={lang === LANGUAGE.ko ? "ko" : "en"}
+            onChange={(e) => handleChangeSelect("lang", e.target.value)}
+          >
+            <option value="ko">한국어</option>
+            <option value="en">ENGLISH</option>
+          </select>
         </div>
         <div className="flex">
-          <div>{HP_MULTI_LANG_HANDLER(lang, "정렬", "Sort")}</div>
-          <div>
-            <select
-              name="favorite-cuisine"
-              aria-label=""
-              required
-              onChange={(e) => handleChangeSelect("order", e.target.value)}
-            >
-              <option disabled value="">
-                {HP_MULTI_LANG_HANDLER(
-                  lang,
-                  "정렬방식을 선택하세요.",
-                  "Choose a Sorting method",
-                )}
-              </option>
-              <option value="desc" selected={order === ORDER.desc}>
-                {HP_MULTI_LANG_HANDLER(lang, "최근 순", "recent")}
-              </option>
-              <option value="asc" selected={order === ORDER.asc}>
-                {HP_MULTI_LANG_HANDLER(lang, "오래된 순", "old")}
-              </option>
-            </select>
+          <div className="flex-1">
+            {HP_MULTI_LANG_HANDLER(lang, "정렬", "Sort")}
           </div>
+          <select
+            className="flex-1 min-w-[120px]"
+            aria-label=""
+            defaultValue={order === ORDER.desc ? "recent" : "asc"}
+            onChange={(e) => handleChangeSelect("order", e.target.value)}
+          >
+            <option value="desc">
+              {HP_MULTI_LANG_HANDLER(lang, "최근 순", "recent")}
+            </option>
+            <option value="asc">
+              {HP_MULTI_LANG_HANDLER(lang, "오래된 순", "old")}
+            </option>
+          </select>
         </div>
       </div>
     </div>
